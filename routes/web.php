da1918d0bc85305c844d6a823420dbcb1bfdaaa2
juanfarias <juanfarias580@gmail.com>
2022-04-11 09:31:35 -0300

    refatoração

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 $router->get('/', 'HomeController@index');
 
 //CRUD
-$router->post('/pessoas/create', 'HomeController@create');
-$router->post('/pessoas/read', 'HomeController@read');
-$router->post('/pessoas/update', 'HomeController@update');
-$router->post('/pessoas/delete', 'HomeController@delete');
+$router->post('/pessoas/create', ['as' => 'create', 'uses' => 'HomeController@create']);
+$router->post('/pessoas/read', ['as' => 'read', 'uses' => 'HomeController@read']);
+$router->post('/pessoas/update', ['as' => 'update', 'uses' => 'HomeController@update']);
+$router->post('/pessoas/delete', ['as' => 'delete', 'uses' => 'HomeController@delete']);
